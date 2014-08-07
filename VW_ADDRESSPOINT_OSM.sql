5943aeae01fca841baf99490fbad21f11f113012
Elliott Plack <elliott.plack@gmail.com>
2014-08-07 15:23:32 -0400

    removed junk, unabbrev things
    removed parts unneeded and decoded abbreviated things

--- VW_ADDRESSPOINT_OSM.sql
+++ VW_ADDRESSPOINT_OSM.sql
@@ -1,20 +1,8 @@
 SELECT 
 ADP.SHAPE,
 AD.OBJECTID,
-AD.ADDRESS_ID,
-ADP.ADDRPT_ID,
-ADP.TAXACCTID,
-AD.TAXACCTID AS ADDRESS_TAXACCTID,
-AD.ST_NUMBER,
-AD.ST_NUMSUFFIX,
-DECODE(SN.ST_PREMOD, '1', 'OLD', '2', 'NEW') AS ST_PREMOD,
-SN.ST_PREFIXDIR,
-SN.ST_PREFIXTYPE,
-SN.ST_NAME,
-SN.ST_TYPE,
-SN.ST_SUFFIXDIR,
-DECODE(SN.ST_POSTMOD, '1', 'EXTENDED') AS ST_POSTMOD,
-TRIM(CONCAT(AD.ST_NUMBER || ' ',
+TRIM(CONCAT(AD.ST_NUMBER || ' ',(TRIM(AD.ST_NUMSUFFIX) ||' ')))
+addr_housenumber,
 TRIM(
   CONCAT(
    TRIM(
@@ -28,65 +16,61 @@ TRIM(
            TRIM(
             CONCAT(
              TRIM(
-              CONCAT(TRIM(AD.ST_NUMSUFFIX) ||' ',TRIM(DECODE(SN.ST_PREMOD, '1', 'OLD', '2', 'NEW')))
+              DECODE(SN.ST_PREMOD, '1', 'OLD', '2', 'NEW')
               ) || ' ',TRIM(SN.ST_PREFIXDIR)
              )
             ) || ' ',TRIM(SN.ST_PREFIXTYPE)
            )
           ) || ' ',TRIM(SN.ST_NAME)
          )
-        ) || ' ',TRIM(SN.ST_TYPE)
+        ) || ' ',TRIM(
+                  DECODE(SN.ST_TYPE,
+                  'ALY','ALLEY',
+                  'AVE','AVENUE',
+                  'BLVD','BOULEVARD',
+                  'BRG','BRIDGE',
+                  'CIR','CIRCLE',
+                  'CMN','COMMON',
+                  'CT','COURT',
+                  'CTR','CENTER',
+                  'DR','DRIVE',
+                  'DRWY','DRIVEWAY',
+                  'EXPY','EXPRESSWAY',
+                  'GRN','GREEN',
+                  'GRTH','GARTH',
+                  'HWY','HIGHWAY',
+                  'LN','LANE',
+                  'LOOP','LOOP',
+                  'MALL','MALL',
+                  'MEWS','MEWS',
+                  'PARK','PARK',
+                  'PATH','PATH',
+                  'PIKE','PIKE',
+                  'PKWY','PARKWAY',
+                  'PLZ','PLAZA',
+                  'RD','ROAD',
+                  'ROW','ROW',
+                  'SQ','SQUARE',
+                  'ST','STREET',
+                  'TER','TERRACE',
+                  'TRL','TRAIL',
+                  'WALK','WALK',
+                  'WAY','WAY',
+                  'PL','PLACE')
+                 )
        )
       ) || ' ',TRIM(SN.ST_SUFFIXDIR)
      )
     ) || ' ',TRIM(DECODE(SN.ST_POSTMOD, '1', 'EXTENDED'))
    )
   )
-)
-)ADDRLABEL,
-AD.STATE,
-Communityname.COUNTY_PREF,
-Communityname.POSTAL_PREF AS CITY_POSTAL,
-AD.ZIP,
-AD.ZIP4,
+addr_street,
+AD.STATE AS addr_state,
+Communityname.COUNTY_PREF AS addr_city,
+AD.ZIP AS addr_postal,
 DECODE(AD.STATUS, 'A', 'ACTIVE',
-                                                                                'I', 'INACTIVE',
-                                                                                'U', 'UNKNOWN') AS STATUS,
-DECODE(AD.ADDRESSUSE, '2010', 'COMMERCIAL',
-                '2020', 'INSTITUTIONAL',
-                '2001', 'RESIDENTIAL LOW DENSITY',
-                '2002', 'RESIDENTIAL HIGH DENSITY',
-                '2030', 'BUSINESS',
-                '2040', 'INDUSTRIAL',
-                '2099', 'OTHER',
-                '2097', 'PRIVATE VACANT',
-                '2098', 'PUBLIC VACANT',
-                '2715', 'STORM WATER MANAGEMENT',
-                '2900', 'INFRASTRUCTURE') AS ADDRESSUSE,
-DECODE(ADP.BLDG_CODE, '2010', 'COMMERCIAL',
-                '2020', 'INSTITUTIONAL',
-                '2001', 'RESIDENTIAL LOW DENSITY',
-                '2002', 'RESIDENTIAL HIGH DENSITY',
-                '2030', 'BUSINESS',
-                '2040', 'INDUSTRIAL',
-                '9999', 'UNDER CONSTRUCTION',
-                '2099', 'OTHER',
-                '2097', 'PRIVATE VACANT',
-                '2098', 'PUBLIC VACANT',
-                '2715', 'STORM WATER MANAGEMENT',
-                '2900', 'INFRASTRUCTURE') AS BLDG_CODE,
-ADP.ADC,
-ADP.ADC_NEW,
-ADP.COUNCILMANIC_DISTRICT,
-ADP.ELECTION_DISTRICT,
-ADP.FIREBOX,
-ADP.POL_RPTAREA,
-ADP.LATITUDE,
-ADP.LONGITUDE,
-AD.PARENT_ID,
-AD.STNAME_ID,
-ADP.PIN,
-ADP.VERIFICATION
+ 'I', 'INACTIVE',
+ 'U', 'UNKNOWN') AS STATUS
 FROM AddressPoint ADP,Address AD,StreetName SN,CommunityName
 WHERE ADP.ADDRPT_ID (+) = AD.ADDRPT_ID
 AND SN.STNAME_ID = AD.STNAME_ID
